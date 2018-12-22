814ab5efbc13b19835ab2a5a3252121647f6235a
Noëlle Anthony <noelle.d.anthony@gmail.com>
2018-12-22 14:20:00 -0500

    Changed test.png to multicolor image, changed code slightly

--- stitchify.py
+++ stitchify.py
@@ -11,11 +11,9 @@ for i in range(h):
 	for j in range(w):
 		c = "".join(["{}{}".format(hex(x//16).split('x')[-1], hex(x%16).split('x')[-1]) for x in list(img.getpixel((j,i)))])
 		d = " "
-		if c in symbols.keys():
-			d = symbols[c]
-		else:
+		if c not in symbols.keys():
 			symbols[c] = characters[0]
 			characters = characters[1:]
-			d = symbols[c]
+		d = symbols[c]
 		print(d, end="")
 	print()
